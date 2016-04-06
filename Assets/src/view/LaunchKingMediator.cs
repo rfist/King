9384121d3d5a64f9451edd6cd51ad41fa35cdf43
Vladimir Budachev <theseuship@gmail.com>
2016-04-07 00:33:42 +0300

    changed main menu, add sound switcher

---
+++ Assets/src/view/LaunchKingMediator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LaunchKingMediator : MonoBehaviour {
+
+    private bool IsSelected = false;
+    private SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        renderer.enabled = IsSelected;
+	}
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!IsSelected)
+            {
+                IsSelected = true;
+            }
+            else
+            {
+                startGame();
+            }
+        }
+    }
+
+    void startGame()
+    {
+        SceneManager.LoadScene("SelectPlayer");
+    }
+}
---
+++ Assets/src/view/SoundMediator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundMediator : MonoBehaviour
+{
+
+    public GameObject Icon;
+    public GameObject IconDisabled;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IconDisabled.SetActive(!Config.IS_SOUNDS_ON);
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Config.IS_SOUNDS_ON = !Config.IS_SOUNDS_ON;
+        }
+    }
+}
